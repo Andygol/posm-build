8d0198c6f7740d6199153ac62d30c78552ada349
Seth Fitzsimmons <seth@mojodna.net>
2016-05-30 11:48:23 -0700

    Make posm the default server

--- kickstart/scripts/nginx-deploy.sh
+++ kickstart/scripts/nginx-deploy.sh
@@ -6,7 +6,7 @@ deploy_nginx_ubuntu() {
   apt-get install --no-install-recommends -y nginx
   expand etc/nginx-posm.conf /etc/nginx/sites-available/posm
   rm -f /etc/nginx/sites-enabled/default
-  ln -s -f ../sites-available/posm /etc/nginx/sites-enabled/
+  ln -s -f ../sites-available/posm /etc/nginx/sites-enabled/00posm
   service nginx restart
 
   mkdir -p /opt/posm-www
